a645d144e7bb67f87b4c7567a6787e6bca0b91fe
Mikaela Sano <87887312+mikaela-sano@users.noreply.github.com>
2022-04-07 11:08:04 +1000

    Update hashtable_dictionary.py
    Completed hashtable

--- dictionary/hashtable_dictionary.py
+++ dictionary/hashtable_dictionary.py
@@ -16,7 +16,12 @@ class HashTableDictionary(BaseDictionary):
         construct the data structure to store nodes
         @param words_frequencies: list of (word, frequency) to be stored
         """
-        # TO BE IMPLEMENTED
+        self.thisDict = {}
+        for i in words_frequencies:
+            wordCat = i.word
+            freqCat = i.frequency
+            self.thisDict.update({wordCat: freqCat})
+
 
     def search(self, word: str) -> int:
         """
@@ -24,8 +29,10 @@ class HashTableDictionary(BaseDictionary):
         @param word: the word to be searched
         @return: frequency > 0 if found and 0 if NOT found
         """
-        # TO BE IMPLEMENTED
-        # place holder for return
+
+        for i in self.thisDict:
+            if i == word:
+                return(self.thisDict[i])
         return 0
 
     def add_word_frequency(self, word_frequency: WordFrequency) -> bool:
@@ -34,8 +41,13 @@ class HashTableDictionary(BaseDictionary):
         @param word_frequency: (word, frequency) to be added
         :return: True whether succeeded, False when word is already in the dictionary
         """
-        # TO BE IMPLEMENTED
-        # place holder for return
+        for i in self.thisDict:
+            if i == word_frequency.word:
+                return False
+            else:
+                self.thisDict.update({word_frequency.word: word_frequency.frequency})
+                return True
+
         return False
 
     def delete_word(self, word: str) -> bool:
@@ -44,16 +56,29 @@ class HashTableDictionary(BaseDictionary):
         @param word: word to be deleted
         @return: whether succeeded, e.g. return False when point not found
         """
-        # TO BE IMPLEMENTED
-        # place holder for return
+        for i in self.thisDict:
+            if i == word:
+                del self.thisDict[i]
+                return True
+
         return False
 
-    def autocomplete(self, word: str) -> [str]:
+    def autocomplete(self, prefix_word: str) -> [WordFrequency]:
         """
         return a list of 3 most-frequent words in the dictionary that have 'word' as a prefix
         @param word: word to be autocompleted
         @return: a list (could be empty) of (at most) 3 most-frequent words with prefix 'word'
         """
-        # TO BE IMPLEMENTED
-        # place holder for return
-        return []
+        newDict = {}
+        finalList = []
+        for i in self.thisDict:
+            if i.startswith(prefix_word):
+                newDict.update({i: self.thisDict[i]})
+
+        newDict = dict(sorted(newDict.items(), key=lambda item: item[1]))
+
+        for i in list(reversed(list(newDict)))[0:3]:
+            wordItem = WordFrequency(i, newDict[i])
+            finalList.append(wordItem)
+
+        return(finalList)
